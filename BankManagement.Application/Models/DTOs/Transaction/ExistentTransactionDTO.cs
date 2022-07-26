4a37ad384631983c787db0927bf5aaff221876fa
caicedoluiss <lusecapi@gmail.com>
2022-07-26 14:35:15 -0500

    Fix: Transaction DTOs
    - ExistentTransactionDTO with Type property readonly
    - UpdatingTransactionDTO with no Type property

--- BankManagement.Application/Models/DTOs/Transaction/ExistentTransactionDTO.cs
+++ BankManagement.Application/Models/DTOs/Transaction/ExistentTransactionDTO.cs
@@ -1,11 +1,12 @@
 using System;
+using BankManagement.Domain;
 
 namespace BankManagement.Application;
 
 public class ExistentTransactionDTO
 {
   public DateTime Date { get; set; }
-  public string? Type { get; set; }
+  public string Type => Value == 0 ? TransactionType.Check.ToString() : Value < 0 ? TransactionType.Debit.ToString() : TransactionType.Credit.ToString();
   public decimal Value { get; set; }
   public decimal Balance { get; set; }
   public int AccountId { get; set; }
--- BankManagement.Application/Models/DTOs/Transaction/UpdatingTransactionDTO.cs
+++ BankManagement.Application/Models/DTOs/Transaction/UpdatingTransactionDTO.cs
@@ -1,9 +1,10 @@
+using BankManagement.Domain;
+
 namespace BankManagement.Application;
 
 public class UpdatingTransactionDTO
 {
   public string? Date { get; set; }
-  public string? Type { get; set; }
   public decimal Value { get; set; }
   public decimal Balance { get; set; }
   public int AccountId { get; set; }
