b513835fa5aa0cbe357a77951341af735c49ba2b
caicedoluiss <lusecapi@gmail.com>
2022-08-03 17:43:36 -0500

    Added Delete customer use case logic

--- BankManagement.Application/UseCases/Customers/Commands/DeleteCustomerRequestCommandHandler.cs
+++ BankManagement.Application/UseCases/Customers/Commands/DeleteCustomerRequestCommandHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
@@ -6,8 +7,31 @@ namespace BankManagement.Application;
 
 public class DeleteCustomerRequestCommandHandler : IRequestHandler<DeleteCustomerRequestCommand, ExistentCustomerDTO>
 {
-  public Task<ExistentCustomerDTO> Handle(DeleteCustomerRequestCommand request, CancellationToken cancellationToken)
+  private readonly IUnitOfWork unitOfwork;
+
+  private readonly ICustomerMappingProfile mappingProfile;
+
+  public DeleteCustomerRequestCommandHandler(IUnitOfWork unitOfwork, ICustomerMappingProfile mappingProfile)
   {
-    throw new System.NotImplementedException();
+    this.unitOfwork = unitOfwork;
+    this.mappingProfile = mappingProfile;
+  }
+
+
+
+  public async Task<ExistentCustomerDTO> Handle(DeleteCustomerRequestCommand request, CancellationToken cancellationToken)
+  {
+    if (request.CustomerId < 1) throw new ArgumentException(nameof(request.CustomerId));
+
+    var customer = await unitOfwork.CustomersRepo.Get(request.CustomerId);
+
+    if (customer is null) throw new ArgumentException(nameof(request.CustomerId));
+
+    ExistentCustomerDTO result = mappingProfile.Map(customer);
+
+    unitOfwork.CustomersRepo.Delete(customer);
+    _ = await unitOfwork.Complete();
+
+    return result;
   }
 }
