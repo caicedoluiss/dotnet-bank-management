283cf803faa5f4ba15fcc0c61b9ff67ddd29a223
caicedoluiss <lusecapi@gmail.com>
2022-08-03 18:37:36 -0500

    Fix request and handler to return nullable

--- BankManagement.Application/Models/Requests/Account/Queries/GetAccountByIdRequest.cs
+++ BankManagement.Application/Models/Requests/Account/Queries/GetAccountByIdRequest.cs
@@ -2,7 +2,7 @@ using MediatR;
 
 namespace BankManagement.Application;
 
-public class GetAccountByIdRequest : IRequest<ExistentAccountDTO>
+public class GetAccountByIdRequest : IRequest<ExistentAccountDTO?>
 {
   public int AccountId { get; set; }
   public bool RetreiveCustomerInfo { get; set; }
--- BankManagement.Application/UseCases/Accounts/Queries/GetAccountByIdRequestHandler.cs
+++ BankManagement.Application/UseCases/Accounts/Queries/GetAccountByIdRequestHandler.cs
@@ -4,9 +4,9 @@ using MediatR;
 
 namespace BankManagement.Application;
 
-public class GetAccountByIdRequestHandler : IRequestHandler<GetAccountByIdRequest, ExistentAccountDTO>
+public class GetAccountByIdRequestHandler : IRequestHandler<GetAccountByIdRequest, ExistentAccountDTO?>
 {
-  public Task<ExistentAccountDTO> Handle(GetAccountByIdRequest request, CancellationToken cancellationToken)
+  public Task<ExistentAccountDTO?> Handle(GetAccountByIdRequest request, CancellationToken cancellationToken)
   {
     throw new System.NotImplementedException();
   }
