cebd58750112d0df57c348760516b05a0c4d5f29
caicedoluiss <lusecapi@gmail.com>
2022-08-04 17:02:11 -0500

    Added update account use case logic

--- BankManagement.Application/UseCases/Accounts/Commands/UpdateAccountRequestCommandHandler.cs
+++ BankManagement.Application/UseCases/Accounts/Commands/UpdateAccountRequestCommandHandler.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
@@ -6,8 +8,35 @@ namespace BankManagement.Application;
 
 public class UpdateAccountRequestCommandHandler : IRequestHandler<UpdateAccountRequestCommand, ExistentAccountDTO>
 {
-  public Task<ExistentAccountDTO> Handle(UpdateAccountRequestCommand request, CancellationToken cancellationToken)
+  private readonly IUnitOfWork unitOfwork;
+  private readonly IAccountMappingProfile mappingProfile;
+
+  public UpdateAccountRequestCommandHandler(IUnitOfWork unitOfwork, IAccountMappingProfile mappingProfile)
   {
-    throw new System.NotImplementedException();
+    this.unitOfwork = unitOfwork;
+    this.mappingProfile = mappingProfile;
+  }
+
+  public async Task<ExistentAccountDTO> Handle(UpdateAccountRequestCommand request, CancellationToken cancellationToken)
+  {
+    if (request.AccountInfo is null) throw new ArgumentException(nameof(request.AccountInfo));
+
+    var validator = new NewAccountDTOValidator(unitOfwork);
+    var validationResult = await validator.ValidateAsync(request.AccountInfo, cancellationToken);
+
+    if (!validationResult.IsValid) throw new ArgumentException(string.Join(", ", validationResult.Errors.Select(x => x.PropertyName)));
+
+    var account = await unitOfwork.AccountsRepo.Get(request.AccountId, request.RetrieveCustomerInfo);
+
+    if (account is null) throw new ArgumentException(nameof(request.AccountId));
+
+    _ = mappingProfile.Map(request.AccountInfo, account);
+
+    unitOfwork.AccountsRepo.Update(account);
+    _ = await unitOfwork.Complete();
+
+    ExistentAccountDTO result = mappingProfile.Map(account);
+
+    return result;
   }
 }
